96a152556ced20888c17ebd0d79ef97fe7c18d73
Angel <angelkysinn@gmail.com>
2021-03-16 17:12:06 -0700

    Added appointment data to render schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import Appointment from "components/Appointment";
 import "components/Application.scss";
 import DayList from "./DayList";
 
@@ -21,8 +21,67 @@ const days = [
   },
 ];
 
+const appointmentsArray = [
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Student 1",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      },
+    },
+  },
+  {
+    id: 3,
+    time: "2pm",
+    interview: {
+      student: "Student 2",
+      interviewer: {
+        id: 2,
+        name: "Tori Malcolm",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      },
+    },
+  },
+  {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Student 3",
+      interviewer: {
+        id: 3,
+        name: "Mildred Nazir",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      },
+    },
+  },
+  {
+    id: 5,
+    time: "4pm",
+    interview: {
+      student: "Student 4",
+      interviewer: {
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      },
+    },
+  },
+];
+
 export default function Application(props) {
   const [day, setDay] = useState("Monday");
+
+  const appointments = appointmentsArray.map((appointment) => {
+    return <Appointment key={appointment.id} {...appointment} />;
+  });
 
   return (
     <main className="layout">
@@ -43,7 +102,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments}
+        <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
