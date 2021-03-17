63ad71614a9c855ec1db03c5bdbbed7164132871
Angel <angelkysinn@gmail.com>
2021-03-17 15:37:39 -0700

    Combined state in Application component

--- src/components/Application.js
+++ src/components/Application.js
@@ -62,17 +62,21 @@ const appointmentsArray = [
 ];
 
 export default function Application(props) {
-  const [day, setDay] = useState("Monday");
-  const [days, setDays] = useState([]);
+  const [state, setState] = useState({
+    day: "Monday",
+    days: [],
+    // appointments: {},
+  });
 
   const appointments = appointmentsArray.map((appointment) => {
     return <Appointment key={appointment.id} {...appointment} />;
   });
 
+  const setDay = (day) => setState({ ...state, day });
+  const setDays = (days) => setState((prev) => ({ ...prev, days }));
+
   useEffect(() => {
-    axios.get("/api/days").then((response) => {
-      setDays(response.data);
-    });
+    axios.get("/api/days").then((response) => setDays(response.data));
   }, []);
 
   return (
@@ -85,7 +89,7 @@ export default function Application(props) {
         />
         <hr className="sidebar__separator sidebar--centered" />
         <nav className="sidebar__menu">
-          <DayList days={days} day={day} setDay={setDay} />
+          <DayList days={state.days} day={state.day} setDay={setDay} />
         </nav>
         <img
           className="sidebar__lhl sidebar--centered"
