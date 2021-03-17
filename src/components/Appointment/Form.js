60c73fadb91d46f95610c94c61e3c0e9fbb65ef1
Angel <angelkysinn@gmail.com>
2021-03-16 18:15:41 -0700

    Refactoring code for controlled components

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -22,7 +22,7 @@ export default function Form(props) {
         <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
-            name={name}
+            value={name}
             type="text"
             placeholder="Enter Student Name"
             onChange={(event) => setName(event.target.value)}
@@ -30,8 +30,8 @@ export default function Form(props) {
         </form>
         <InterviewerList
           interviewers={props.interviewers}
-          interviewer={interviewer}
-          setInterviewer={setInterviewer}
+          value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
