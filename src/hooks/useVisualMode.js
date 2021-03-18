87149289924f91fec522c448c3910474ce2258a8
Angel <angelkysinn@gmail.com>
2021-03-18 15:25:31 -0700

    Refactored useVisualMode function

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial);
@@ -7,15 +7,12 @@ export default function useVisualMode(initial) {
   const transition = (newMode, replace = false) => {
     if (replace) {
       setHistory((prev) => {
-        const newHistory = [...prev].slice(0, -1);
-        // newHistory.pop();
-        return newHistory;
+        return [...prev].slice(0, -1);
       });
     }
     setHistory((prev) => {
       setMode(newMode);
-      const newHistory = [...prev, newMode];
-      return newHistory;
+      return [...prev, newMode];
     });
   };
 
@@ -23,7 +20,6 @@ export default function useVisualMode(initial) {
     if (history.length > 1) {
       setHistory((prev) => {
         const newHistory = [...prev].slice(0, -1);
-        // newHistory.pop();
         setMode(newHistory.slice(-1)[0]);
         return newHistory;
       });
