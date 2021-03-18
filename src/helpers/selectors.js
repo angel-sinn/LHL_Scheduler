3ff9d10ea6df5ffbdd769cbe4e5f611040083a20
Angel <angelkysinn@gmail.com>
2021-03-18 12:52:08 -0700

    Added getInterview helper function and tested it

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -3,11 +3,26 @@ export function getAppointmentsForDay(state, day) {
 
   const filteredDay = state.days.filter((dayObj) => dayObj.name === day);
 
-  if (filteredDay.length > 0) {
-    for (const appointmentId of filteredDay[0].appointments) {
-      appointmentsArr.push(state.appointments[appointmentId]);
-    }
+  if (!filteredDay.length) {
+    return appointmentsArr;
+  }
+
+  for (const appointmentId of filteredDay[0].appointments) {
+    appointmentsArr.push(state.appointments[appointmentId]);
   }
 
   return appointmentsArr;
+}
+
+export function getInterview(state, interview) {
+  if (!interview) {
+    return null;
+  }
+
+  const interviewObj = {
+    student: interview.student,
+    interviewer: state.interviewers[interview.interviewer],
+  };
+
+  return interviewObj;
 }
