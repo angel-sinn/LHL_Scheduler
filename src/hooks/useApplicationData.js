86d5c2147a993ef6e3d7a2592f14f193ef928c41
Angel <angelkysinn@gmail.com>
2021-03-21 14:10:55 -0700

    Implemented websocket

--- src/hooks/useApplicationData.js
+++ src/hooks/useApplicationData.js
@@ -5,6 +5,32 @@ const SET_DAY = "SET_DAY";
 const SET_APPLICATION_DATA = "SET_APPLICATION_DATA";
 const SET_INTERVIEW = "SET_INTERVIEW";
 
+//check for available slots in a day
+const availableSlots = (day, appointments) => {
+  let count = 0;
+  for (const id of day.appointments) {
+    const appointment = appointments[id];
+    if (!appointment.interview) {
+      count++;
+    }
+  }
+  return count;
+};
+
+// function to check for available slots and updates info
+const updateSpots = function (dayName, days, appointments) {
+  const day = days.find((item) => item.name === dayName);
+  const slots = availableSlots(day, appointments);
+
+  const newDaysArr = days.map((item) => {
+    if (item.name === dayName) {
+      return { ...item, spots: slots };
+    }
+    return item;
+  });
+  return newDaysArr;
+};
+
 function reducer(state, action) {
   switch (action.type) {
     case SET_DAY:
@@ -17,7 +43,24 @@ function reducer(state, action) {
         interviewers: action.interviewers,
       };
     case SET_INTERVIEW:
-      return { ...state, appointments: action.appointments, days: action.days };
+      const { id, interview } = action;
+
+      // replace current value of interview key with new value
+      const appointment = {
+        ...state.appointments[id],
+        interview,
+      };
+
+      // replace existing record with matching id
+      const appointments = {
+        ...state.appointments,
+        [id]: appointment,
+      };
+
+      const days = updateSpots(state.day, state.days, appointments);
+
+      return { ...state, appointments, days };
+
     default:
       throw new Error(
         `Tried to reduce with unsupported action type: ${action.type}`
@@ -52,79 +95,51 @@ export default function useApplicationData() {
     });
   }, []);
 
+  // setting up websocket
+  useEffect(() => {
+    const socket = new WebSocket(process.env.REACT_APP_WEBSOCKET_URL);
+    socket.onopen = () => {
+      socket.send("ping");
+    };
+
+    socket.onmessage = (event) => {
+      // console.log("message received: " + event.data);
+
+      const data = JSON.parse(event.data);
+      // console.log(event.data);
+      if (typeof data === "object" && data.type === "SET_INTERVIEW") {
+        dispatch(data);
+      }
+    };
+
+    // cleanup function
+    return () => socket.close();
+
+    // add dispatch as dependency!
+  }, [dispatch]);
+
   const setDay = (day) => dispatch({ type: SET_DAY, day });
 
   const bookInterview = (id, interview) => {
-    // replace current value of interview key with new value
-    const appointment = {
-      ...state.appointments[id],
-      interview: { ...interview },
-    };
-
-    // replace existing record with matching id
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-
-    const days = updateSpots(state.day, state.days, appointments);
-
     // make request to API to update appointment with new interview info with axios
-    return axios.put(`/api/appointments/${id}`, appointment).then((res) => {
-      dispatch({ type: SET_INTERVIEW, appointments, days });
-    });
-  };
-
-  const cancelInterview = (id) => {
-    // set current value of interview key to null
-    const appointment = {
-      ...state.appointments[id],
-      interview: null,
-    };
-
-    // replace existing record with matching id
-    const appointments = {
-      ...state.appointments,
-      [id]: appointment,
-    };
-
-    const days = updateSpots(state.day, state.days, appointments);
-
-    // make request to API to update/delete appointment with new interview info with axios
-    return axios.delete(`/api/appointments/${id}`).then((res) => {
+    return axios.put(`/api/appointments/${id}`, { interview }).then((res) => {
       dispatch({
         type: SET_INTERVIEW,
-        appointments,
-        days,
+        id,
+        interview,
       });
     });
   };
 
-  //check for available slots in a day
-  const availableSlots = (day, appointments) => {
-    let count = 0;
-    for (const id of day.appointments) {
-      const appointment = appointments[id];
-      if (!appointment.interview) {
-        count++;
-      }
-    }
-    return count;
-  };
-
-  // function to check for available slots and updates info
-  const updateSpots = function (dayName, days, appointments) {
-    const day = days.find((item) => item.name === dayName);
-
-    const slots = availableSlots(day, appointments);
-
-    const newDaysArr = days.map((item) => {
-      if (item.name === dayName) {
-        return { ...item, spots: slots };
-      }
-      return item;
+  const cancelInterview = (id) => {
+    // make request to API to update/delete appointment with new interview info with axios
+    return axios.delete(`/api/appointments/${id}`).then((res) => {
+      dispatch({
+        type: SET_INTERVIEW,
+        id,
+        interview: null,
+      });
     });
-    return newDaysArr;
   };
 
   return { state, setDay, bookInterview, cancelInterview };
