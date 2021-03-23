d608e576b988156096b72fed366cae830aae9c34
Angel <angelkysinn@gmail.com>
2021-03-23 16:08:32 -0700

    Added additional comments for future reference

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -7,7 +7,6 @@ import {
   waitForElement,
   fireEvent,
   getByText,
-  prettyDOM,
   getAllByTestId,
   getByAltText,
   getByPlaceholderText,
@@ -30,27 +29,36 @@ describe("Application", () => {
   });
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
+    // 1. Render the Application.
     const { container, debug } = render(<Application />);
 
+    // 2. Wait until the text "Archie Cohen" is displayed.
     await waitForElement(() => getByText(container, "Archie Cohen"));
 
     const appointments = getAllByTestId(container, "appointment");
     const appointment = appointments[0];
 
+    // 3. Click the "Add" button on the booked appointment.
     fireEvent.click(getByAltText(appointment, "Add"));
 
+    // 4. Enter name "Lydia Miller-Jones" into input with placeholder "Enter Student Name"
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" },
     });
 
+    // 5. Click first interviewer in list
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
 
+    // 6. Click "save" button on same appointment
     fireEvent.click(getByText(appointment, "Save"));
 
+    // 7. Check that the element with the text "Saving" is displayed.
     expect(getByText(appointment, "Saving")).toBeInTheDocument();
 
+    // 8. Wait until element with text "Lydia Miller-Jones" is displayed
     await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
 
+    // 9. Check that DayListItem with the text "Monday" also has the text "no spots remaining"
     const day = getAllByTestId(container, "day").find((day) =>
       queryByText(day, "Monday")
     );
